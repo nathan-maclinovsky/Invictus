eb6ee22205899fdfe4bef762b0c15ef1b06f5801
ozamabinllama <nathanmaclinovsky@gmail.com>
2023-10-08 22:56:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ tileset.src = 'fantasyhextiles_v3.png';
 const CANVAS_WIDTH = canvas.width = 1000;
 const CANVAS_HEIGHT = canvas.height =1000;
 
+
 function start(){
     document.getElementById("beginbutton").style.display = "none";
     console.log("ben");
