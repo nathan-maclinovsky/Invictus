217b4e31d249ea3a1c374b43ba6e2202c973a8a1
ozamabinllama <nathanmaclinovsky@gmail.com>
2023-10-13 15:50:46 -0500

    Map Gen update

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
 /**@type {HTMLCancasElement} */
+
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
+const set2 = new Image();
+set2.src = 'Hex_v01_grid.png';
+
+const tileset = new Image();
+tileset.src = 'hexagonTerrain_sheet.png';
 
 
 const intro1 = new Image();
 intro1.src = 'pixle_art_of_a__1-transformed.png';
 
-const tileset = new Image();
-tileset.src = 'fantasyhextiles_v3.png';
+
+
 const CANVAS_WIDTH = canvas.width = 1000;
 const CANVAS_HEIGHT = canvas.height =1000;
-
+//console.log( mymap.tileList[1].coords[0]);
 
 
 function start(){
     document.getElementById("beginbutton").style.display = "none";
-    console.log("ben");
+
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(tileset,256,288,256,288);
-    requestAnimationFrame(start);  
+    
+    for (let i = 0; i < mymap.tileList.length; i++) {
+        //console.log(mymap.tileList[i].coords)
+        dx =mymap.tileList[i].coords[0];
+        //console.log(mymap.tileList[i].coords[0]);
+        dy =mymap.tileList[i].coords[1];
+        console.log(mymap.tileList[i].coords[1]);
+        ctx.drawImage(set2, 0, 0, 32, 48, dx, dy, 32, 48);
+        
+       
+      } 
+    //ctx.drawImage(set2, 0, 0, 32, 48, 516, 474, 32, 48);
+    //ctx.drawImage(set2, 0, 0, 32, 48, 532, 495, 32, 48);
+    //ctx.drawImage(set2, 0, 0, 32, 48, 500, 495, 32, 48);
+    //ctx.drawImage(set2, 0, 0, 32, 48, 516, 516, 32, 48);
+
+       requestAnimationFrame(start);
+
+   
+    
+    
 }
 
 
+
 function animate(){ 
-    console.log("intro");
+
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
     ctx.drawImage(intro1,0,0);
     requestAnimationFrame(animate);
     
 }
 animate();
+
+
+
+    //ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
+   
+
+
+
+
+
 
 
 
---
+++ tile.js
@@ -0,0 +1,53 @@
+const mymap ={
+    "tileList": [
+        {
+            "type": "ocean",
+            "coords": [
+                484,
+                479
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                516,
+                479
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                500,
+                500
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                532,
+                500
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                468,
+                500
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                516,
+                521
+            ]
+        },
+        {
+            "type": "ocean",
+            "coords": [
+                484,
+                521
+            ]
+        }
+    ]
+};
