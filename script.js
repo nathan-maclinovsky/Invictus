066018ff6cf06d9b2bf22a2b803e8f2f879587b4
Nathan Leo Maclinovsky <nathanmaclinovsky@gmail.com>
2023-11-01 19:05:06 -0500

    Update script.js
    update

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ const intro1 = new Image();
 intro1.src = 'Game assets/pixle_art_of_a__1-transformed.png';
 
 const shroom_sheet = new Image();
-const numberOfEnemies = 1;
+const numberOfEnemies = 10;
 shroom_sheet.src = 'Game assets/Attack3.png';
+
+const necromancer = new Image();
+necromancer.src = 'Game assets/Necromancer_creativekind-Sheet.png';
+
+let gameFrameSpeed = 3;
+
+
 let gameFrame = 0;
 const shroomarray = [];
-
+const necromancerArray = [];
 
 
 
@@ -21,11 +28,11 @@ const CANVAS_WIDTH = canvas.width = 1500;
 const CANVAS_HEIGHT = canvas.height =1500;
 //console.log( mymap.tileList[1].coords[0]);
 class Shroom{
-  constructor(tile){
+  constructor(tileCords){
     
     
-    this.x = 500;
-    this.y = 500;
+    this.x = tileCords[0];
+    this.y = tileCords[1];
     //this.x = tile[0];
     //this.y = tile[1];
     this.frame = 0;
@@ -34,7 +41,7 @@ class Shroom{
     this.spriteHeight= 165;
  }
  update(){
-  if(gameFrame % 10 ===0 ){
+  if(gameFrame % gameFrameSpeed ===0 ){
 
   this.frame>5 ? this.frame = 0 : this.frame++;
  }
@@ -45,12 +52,44 @@ draw(){
 
 
 }
+class Necromancer{
+  constructor(tileCords){
+    this.x = tileCords[0];
+    this.y = tileCords[1];
+    //this.x = tile[0];
+    //this.y = tile[1];
+    this.frame = 0;
+    this.power =  Math.floor(Math.random() * 10) + 1;
+    this.spriteWidth = 160;
+    this.spriteHeight= 128;
+    this.state = 4;
+ }
+ update(){
+  if(gameFrame % gameFrameSpeed ===0 ){
+
+  this.frame>15 ? this.frame = 0 : this.frame++;
+ }
+}
+draw(){
+  //ctx.fillRect(this.x, this.y, this.spriteWidth/4, this.spriteHeight/4); 
+  ctx.drawImage(necromancer,this.frame * this.spriteWidth,this.state*this.spriteHeight,this.spriteWidth,this.spriteHeight,this.x,this.y,this.spriteWidth/4,this.spriteHeight/4);
+  
+  
+}
+
+
+}
 function nobutton(){
   document.getElementById("beginbutton").style.display = "none";
   start();
 }
 for(let i = 0; i< numberOfEnemies; i++){
-  shroomarray.push(new Shroom(mymap.tileList[100]));
+  //var randomNumber = Math.floor(Math.random() * 722) + 0;
+ // shroomarray.push(new Shroom(mymap.tileList[randomNumber].coords));
+  //console.log(mymap.tileList[randomNumber].coords);
+  var randomNumber = Math.floor(Math.random() * 722) + 0;
+  necromancerArray.push(new Necromancer(mymap.tileList[randomNumber].coords));
+  console.log(mymap.tileList[randomNumber].coords);
  
 }
 
@@ -67,22 +106,26 @@ function start(){
             mult =8;
         }
         ctx.drawImage(set2, 32*mult, 0, 32, 48, dx, dy, 32, 48);
+        //ctx.fillRect(dx, dy, 32, 48);
         
        
       } 
-      shroomarray.forEach(Shroom => {
-        Shroom.update();
-        Shroom.draw(); 
+    //   shroomarray.forEach(Shroom => {
+    //     Shroom.update();
+    //     Shroom.draw();
+       
+    // });
+      necromancerArray.forEach(Necromancer => {
+        Necromancer.update();
+        Necromancer.draw();
+        
+         
     });
     requestAnimationFrame(start);
 
 
 
 }
-
-
-
-
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -91,10 +134,8 @@ function getRandomInt(max) {
 
 
 function animate(){ 
-
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
     ctx.drawImage(intro1, canvas.width / 2 - intro1.width / 2,canvas.height / 2 - intro1.height / 2);
-
     requestAnimationFrame(animate);
     
 }
